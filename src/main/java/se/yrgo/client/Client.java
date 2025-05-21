9d06ffb622517a2f06bf1be008c8ef222e584fb5
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-05-21 11:31:00 +0200

    document menu options

--- src/main/java/se/yrgo/client/Client.java
+++ src/main/java/se/yrgo/client/Client.java
@@ -7,6 +7,13 @@ public class Client {
 
 	public static void main(String[] args) {
 
+		// menu options:
+		// 1. show all games
+		// 2. select a game (and show details and their reviews)
+		// 3. write and add a review on a game
+		// 4. remove your own review
+		// 5. update your own review
+
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
